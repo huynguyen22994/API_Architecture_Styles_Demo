26c27bff04106f5cb1ed3cffd8253ec1503971d1
Huy Nguyen <huy.nguyen22994@gmail.com>
2024-03-22 07:09:28 +0700

    implement hero service

---
+++ packages/api-gateway/src/hero/heroes.controller.ts
@@ -0,0 +1,16 @@
+import { Controller } from '@nestjs/common'
+import { GrpcMethod } from '@nestjs/microservices'
+import { ServerUnaryCall, Metadata } from '@grpc/grpc-js'
+import { HeroById, Hero } from './hero'
+
+@Controller()
+export class HeroesController {
+  @GrpcMethod('HeroesService', 'FindOne')
+  findOne(data: HeroById, metadata: Metadata, call: ServerUnaryCall<any, any>): Hero {
+    const items = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' },
+    ];
+    return items.find(({ id }) => id === data.id);
+  }
+}
